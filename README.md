1877c984161b46f98d4f67965b4597a656c634e0
daisuketerashita <ekusiad.tera@icloud.com>
2021-08-01 14:54:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PHP_JavaScript_xycoordinates
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);  /* MAMPの設定次第では記述が必要 1は表示、0は非表示*/
+error_reporting(-1);  /* 0は表示させない、-1はすべて表示 */
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>sortable</title>
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div id="wrapper">
+
+<div id="drag-area">
+drag area
+</div>
+
+</div>
+</body>
+</html>
